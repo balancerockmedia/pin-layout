acd0e3eaa45c15a06a7c46e000cdd72f05aec040
Daniel Johnson <dan@balancerockmedia.com>
2013-03-23 08:33:58 -0400

    edits

--- index.php
+++ index.php
@@ -8,28 +8,28 @@
 <script type="text/javascript" src="js/pinLayout.js"></script>
 <script type="text/javascript">
 
-	$('#container').pinLayout({
-		total_column_width: 200
-	});
-	
+    $('#container').pinLayout({
+        total_column_width: 200
+    });
+    
 </script>
 </head>
 <body>
-	
+    
 <div id="container">
-	<div id="wrapper">
-	<?php
-	
-	// build some divs with random amounts of content to test out the layout
-	
-	$content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue augue, tincidunt eu luctus vitae, cursus quis justo. Suspendisse potenti. Curabitur varius enim tincidunt magna rutrum iaculis. Donec convallis porta dolor, eu ullamcorper nunc convallis sit amet. Curabitur bibendum varius massa, non euismod ante tincidunt et. Aliquam id dui in massa laoreet aliquet ac nec diam. Aenean venenatis blandit facilisis. Donec malesuada, elit sit amet condimentum aliquam, lectus purus tempor felis, eu lacinia tellus ante eget nunc. Vestibulum vitae odio felis. Sed eget odio nulla. Quisque malesuada, tortor sit amet accumsan congue, erat nibh sollicitudin mauris, quis cursus diam tellus eget mi. Curabitur mollis accumsan congue. Duis iaculis velit vehicula nibh ornare faucibus. Morbi libero felis, rutrum at suscipit quis, pulvinar quis turpis. Vestibulum ac lectus egestas felis sagittis fringilla. Duis vitae tellus in mauris iaculis rutrum.';
+    <div id="wrapper">
+    <?php
+    
+    // build some divs with random amounts of content to test out the layout
+    
+    $content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue augue, tincidunt eu luctus vitae, cursus quis justo. Suspendisse potenti. Curabitur varius enim tincidunt magna rutrum iaculis. Donec convallis porta dolor, eu ullamcorper nunc convallis sit amet. Curabitur bibendum varius massa, non euismod ante tincidunt et. Aliquam id dui in massa laoreet aliquet ac nec diam. Aenean venenatis blandit facilisis. Donec malesuada, elit sit amet condimentum aliquam, lectus purus tempor felis, eu lacinia tellus ante eget nunc. Vestibulum vitae odio felis. Sed eget odio nulla. Quisque malesuada, tortor sit amet accumsan congue, erat nibh sollicitudin mauris, quis cursus diam tellus eget mi. Curabitur mollis accumsan congue. Duis iaculis velit vehicula nibh ornare faucibus. Morbi libero felis, rutrum at suscipit quis, pulvinar quis turpis. Vestibulum ac lectus egestas felis sagittis fringilla. Duis vitae tellus in mauris iaculis rutrum.';
 
-	for ($i = 1; $i <= 20; $i++) {
-		echo '<div class="pin" id="pin_'.$i.'">'.substr($content, 0, rand(10, 1000)).'</div>';
-	}
-	
-	?>
-	</div>
+    for ($i = 1; $i <= 20; $i++) {
+        echo '<div class="pin" id="pin_'.$i.'">'.substr($content, 0, rand(10, 1000)).'</div>';
+    }
+    
+    ?>
+    </div>
 </div>
 
 </body>
